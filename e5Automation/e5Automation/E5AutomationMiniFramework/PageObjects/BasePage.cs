42df3be6922f0e145c62e0c60d70b39c668cd5d9
nmartino <nikolas.martino@gmail.com>
2020-07-04 02:49:54 +0200

    change the name to new one

---
+++ e5Automation/e5Automation/E5AutomationMiniFramework/PageObjects/BasePage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinnWorksTestInterview.PageObjects
+{
+    public class BasePage : driverManager
+    {
+        public IWebDriver driver { get; }
+        public WebDriverWait wait = null;
+
+        public BasePage(IWebDriver driver) : base()
+        {
+            this.driver = getDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        public bool testCaseShouldPass(bool tc) {
+
+            return true;
+        }
+
+      public NavigationBarWebObject navigationBarWebObject()
+        {
+
+            return new NavigationBarWebObject(driver);
+        }
+
+        public MiddlePage middlePage()
+        {
+
+            return new MiddlePage(driver);
+        }
+
+
+
+
+
+    }
+}
---
+++ e5Automation/e5Automation/E5AutomationMiniFramework/driverManager.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace LinnWorksTestInterview
+{
+    public class driverManager
+    {
+        IWebDriver driver;
+
+        [BeforeScenario("UI")]
+        public IWebDriver getDriver() {
+            this.driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:59509/");
+            driver.Manage().Window.Maximize();
+            return driver;            
+        }
+    }
+}
